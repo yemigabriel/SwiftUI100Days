a7bfdfb5004ffbd52b96f60f79eeed2d5366e3d2
Yemi Gabriel <yemigabriel@gmail.com>
2021-06-28 10:30:37 +0100

    WeSplit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Yemi Gabriel on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
