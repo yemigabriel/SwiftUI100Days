3f2ed11daf97171831ad65733ede22caf8b8cfc2
Yemi Gabriel <yemigabriel@gmail.com>
2021-07-05 01:06:50 +0100

    BetterRest initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Yemi Gabriel on 7/5/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
