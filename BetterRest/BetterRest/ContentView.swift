51445ce9e8b01e39e63fa6b07abe28ffd4219cd4
Yemi Gabriel <yemigabriel@gmail.com>
2021-07-05 13:27:01 +0100

    BetterRest DatePicker, Dates

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,9 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeupTime = Date()
+    var tomorrow = Date().addingTimeInterval(86400)
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        var components = DateComponents()
+        components.hour = 8
+        components.minute = 15
+        let date = Calendar.current.date(from: components) ?? Date()
+        
+        let component = Calendar.current.dateComponents([.day, .hour, .minute], from: Date())
+        let hour = component.hour ?? 0
+        print(hour)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        let dateString = dateFormatter.string(from: Date())
+        print(dateString)
+        
+        return VStack(spacing:20) {
+                    DatePicker("Select wake up time", selection: .constant(Date())
+                    )
+                    
+                    DatePicker("Choose wake up time", selection: $wakeupTime, in: Date()...tomorrow)
+            
+            DatePicker("Carpe Diem", selection: .constant(date), displayedComponents: .hourAndMinute)
+                }
+                .padding()
+        
     }
 }
 
