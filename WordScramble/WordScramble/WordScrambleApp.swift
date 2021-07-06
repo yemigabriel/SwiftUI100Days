b54aed001ede6c61eb09e3ccfb49148c94ad1e1d
Yemi Gabriel <yemigabriel@gmail.com>
2021-07-06 13:21:14 +0100

    WordScramble Initial commit

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yemi Gabriel on 7/6/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
